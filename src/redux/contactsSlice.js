61a0f824d4a4e204f8ec00bce376f7d0f8672afa
Michelle0805 <vasudaria85@gmail.com>
2024-04-30 14:29:04 +0800

    Need to debug why the page is empty.

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,57 @@
+// src/redux/slices/contactsSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchContacts, addContact, deleteContact } from './operations';
+// Define initial state for contacts
+const initialContactsState = {
+  contacts: [],
+  isLoading: false,
+  error: null,
+};
+
+// Create slice for contacts
+export const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: initialContactsState,
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(fetchContacts.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.contacts = action.payload;
+      })
+      .addCase(fetchContacts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      })
+      .addCase(addContact.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.contacts.push(action.payload);
+      })
+      .addCase(addContact.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      })
+      .addCase(deleteContact.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.contacts = state.contacts.filter(
+          contact => contact.id !== action.payload
+        );
+      })
+      .addCase(deleteContact.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+// Export actions
+export const contactsReducer = contactsSlice.reducer;
